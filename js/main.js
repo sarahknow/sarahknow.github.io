edfbf2926bc7accce64e040713e70fe994dd41af
sarahknow <sarahknow@outlook.com>
2014-11-19 19:15:39 -0500

    Sliding Divs/social icons
    Set sliding divs and adjusted social icons with transitions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var $beyonce = $('.beyonce');
 var $beyonceSection = $('.beyonce-section');
 var $rugbySection = $('.rugby-section');
 var $rugby = $('.rugby')
+var $contactSection = $('.contact-section');
+var $contact = $('.contact')
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -14,3 +16,61 @@ $win.on('scroll', function () {
 $rugbySection.waypoint(function () {
 	$rugby.addClass('js-rugby-fade');
 }, {offset: '50%'});
+
+$contactSection.waypoint(function () {
+	$contact.addClass('js-contact-fade');
+}, {offset: '50%'});
+
+$(document).ready(function(e){
+   $('#dropdownone').on('click',function(){
+      $('.dropdownwrapone').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdowntwo').on('click',function(){
+      $('.dropdownwraptwo').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownthree').on('click',function(){
+      $('.dropdownwrapthree').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownfour').on('click',function(){
+      $('.dropdownwrapfour').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownfive').on('click',function(){
+      $('.dropdownwrapfive').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownsix').on('click',function(){
+      $('.dropdownwrapsix').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownseven').on('click',function(){
+      $('.dropdownwrapseven').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdowneight').on('click',function(){
+      $('.dropdownwrapeight').slideToggle();
+   });
+})
+
+$(document).ready(function(e){
+   $('#dropdownnine').on('click',function(){
+      $('.dropdownwrapnine').slideToggle();
+   });
+})
